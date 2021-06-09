689aaffc3099a0fb181c1415331f0be26b2d1d47
CarlosRenatoSantAnnaRamirez <carlos.r.s.ramirez@gmail.com>
2021-06-09 19:34:51 -0300

    Primeiro commit

---
+++ 008 - 98 - 001/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _008___98___001 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
