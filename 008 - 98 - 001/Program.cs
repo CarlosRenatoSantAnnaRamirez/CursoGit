07e44a9dee2317a22ae47ff2440a623ef9d1814a
CarlosRenatoSantAnnaRamirez <carlos.r.s.ramirez@gmail.com>
2021-06-09 19:36:25 -0300

    Segundo commit

--- 008 - 98 - 001/Program.cs
+++ 008 - 98 - 001/Program.cs
@@ -4,6 +4,7 @@ namespace _008___98___001 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
