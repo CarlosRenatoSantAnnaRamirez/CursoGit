bf919ed2a90a5be0d46827ba8721396364b20037
CarlosRenatoSantAnnaRamirez <carlos.r.s.ramirez@gmail.com>
2021-06-09 19:48:42 -0300

    Acrescentada pergunta correta

--- 008 - 98 - 001/Program.cs
+++ 008 - 98 - 001/Program.cs
@@ -5,6 +5,7 @@ namespace _008___98___001 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
             Console.ReadLine();
         }
     }
