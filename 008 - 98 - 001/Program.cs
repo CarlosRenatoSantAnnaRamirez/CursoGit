03577938c4086315addc0db94878112b9cecffa5
CarlosRenatoSantAnnaRamirez <carlos.r.s.ramirez@gmail.com>
2021-06-09 20:56:33 -0300

    Nova mensagem

--- 008 - 98 - 001/Program.cs
+++ 008 - 98 - 001/Program.cs
@@ -4,8 +4,8 @@ namespace _008___98___001 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia.");
             Console.ReadLine();
         }
     }
